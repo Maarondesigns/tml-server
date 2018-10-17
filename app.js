c06d6491c1cf17c8022d3f87c8df4ba5cc6df6d2
Michael Wilson <maarondesigns@gmail.com>
2018-10-17 04:03:46 -0500

    dotenv commented out

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+//require("dotenv").config();
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
