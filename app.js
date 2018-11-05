0554ddf173566ead710ec58118bbef0f45bb1b72
Michael Wilson <maarondesigns@gmail.com>
2018-11-05 15:46:53 -0600

    cookie bs

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(
   cookieSession({
     maxAge: 24 * 60 * 60 * 1000 * 30,
-    keys: [process.env.COOKIE_KEY]
+    keys: [process.env.COOKIE_KEY],
+    overwrite: true
   })
 );
 
